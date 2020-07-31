3e0c0d3cb93804e01e24984647338b389df7da76
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-07-31 04:13:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
-  case array.length 
-  when 1
-  "#{array[0]}"
-  when 2
-  array[0..1].join(" and ")
-  else 
-  array[0...-1].join(", ") << ", and #{array[-1]}"
-
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+  end
 end
